de84504c9e6e2cea9496f4227a73989e8d50d7cd
Timur Akhmadaliev <timur600@gmail.com>
2023-03-22 00:33:20 +0400

    Added trading cycle

--- src/Trader.cpp
+++ src/Trader.cpp
@@ -35,6 +35,58 @@ std::time_t Trader::GetCurrentTimestamp()
     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
 
+void Trader::StartTrading()
+{
+    doTrade = true;
+    TradingLoop();
+}
+
+void Trader::TradingLoop()
+{    
+    while (doTrade)
+    {
+        std::thread tradingThread(&Trader::TradingCycle, this);
+        tradingThread.join();
+    }
+}
+
+void Trader::TradingCycle()
+{
+    using namespace std::chrono;
+
+    const auto cycleStartTime = system_clock::now();
+    bool didOperation = false;
+    while (!didOperation && duration_cast<seconds>(cycleStartTime - system_clock::now()).count() < idleTimeToSell)
+    {
+        didOperation = Tick();
+        std::this_thread::sleep_for(seconds(tickSeconds));
+    }
+    if (!didOperation)
+        SellCurrency(currencyToBuyOrSellQuantity);
+}
+
+bool Trader::Tick()
+{
+    UpdatePrice();
+    const double diff = CalculatePercentageDiff();
+    if (diff > 0 && diff > profitPercentToBuy)
+    {
+        BuyCurrency(currencyToBuyOrSellQuantity);
+        return true;
+    }
+    if (diff < 0 && diff < -lossPercentToSell)
+    {
+        SellCurrency(currencyToBuyOrSellQuantity);
+        return true;
+    }
+    return false;
+}
+
+double Trader::CalculatePercentageDiff()
+{
+    return (currentPrice - previousTickPrice) / currentPrice;
+}
+
 void Trader::UpdatePrice()
 {
     previousTickPrice = currentPrice;
--- src/Trader.h
+++ src/Trader.h
@@ -9,26 +9,45 @@ namespace BuySellRepeat_NS
 class Trader
 {
     private:
+        // price drop at which we're selling
         double lossPercentToSell;
+        // price rise at which we're buying
         double profitPercentToBuy;
         std::string tradingPair;
+        // time without significant price changes to sell
+        unsigned int idleTimeToSell;
 
         double currentPrice = 0;
         double previousTickPrice;
+        // "resolution"
         int tickSeconds = 1;
-        double currencyQuantity = 0;
+        double currencyToBuyOrSellQuantity = 0;
 
         std::string myCurrencySymbol;
         std::string tradingCurrencySymbol;
         Vallets portfolio;
 
         WebIO& webIO;
+
+        bool doTrade;
+
+        private:
+        void TradingLoop();
+        void TradingCycle();
+        // returns true if sold or bought in this tick
+        bool Tick();
+        double CalculatePercentageDiff();
+
     public:
-        Trader(const std::string& _tradingPair, const double& currencyToBuyQuantity, const double& _lossPercentToSell, const double& _profitPercentToBuy, WebIO& wio) :
-            lossPercentToSell(_lossPercentToSell), profitPercentToBuy(_profitPercentToBuy), tradingPair(_tradingPair), currencyQuantity(currencyToBuyQuantity), webIO(wio) 
+        Trader(const std::string& _tradingPair, const double& currencyToBuyQuantity, const double& _lossPercentToSell, const double& _profitPercentToBuy, const unsigned int& idleTimeToSellSeconds,  WebIO& wio) :
+            lossPercentToSell(_lossPercentToSell), profitPercentToBuy(_profitPercentToBuy), tradingPair(_tradingPair), currencyToBuyOrSellQuantity(currencyToBuyQuantity), idleTimeToSell(idleTimeToSellSeconds), webIO(wio) 
             {
                 std::copy(tradingPair.begin(), tradingPair.begin() + 3, std::back_inserter(myCurrencySymbol));
                 std::copy(tradingPair.begin() + 3, tradingPair.end(), std::back_inserter(tradingCurrencySymbol));
+                GetValletDataFromServer();
+                UpdatePrice();
+                if (currencyToBuyOrSellQuantity / currentPrice > GetMyCurrencyQuantity())
+                    throw std::logic_error("Trying to buy " + std::to_string(currencyToBuyOrSellQuantity) + " " + tradingCurrencySymbol + " for " + std::to_string(currencyToBuyOrSellQuantity / currentPrice) + " " + myCurrencySymbol + ", but have only " + std::to_string(GetMyCurrencyQuantity()) + " " + myCurrencySymbol);
             };
         ~Trader() = default;
 
@@ -57,6 +76,12 @@ class Trader
         std::string BuyCurrenctyForAll();
 
         static std::time_t GetCurrentTimestamp();
+
+        void StartTrading();
+        void StopTrading()
+        {
+            doTrade = false;
+        }
 };
 
 }
