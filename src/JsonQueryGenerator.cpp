e66e7eae49add12a323e50ede8b562e24a8132f4
Timur Akhmadaliev <timur600@gmail.com>
2023-03-21 21:47:55 +0400

    Signature now sorts parameters alphabetically

--- src/JsonQueryGenerator.cpp
+++ src/JsonQueryGenerator.cpp
@@ -21,20 +21,33 @@ void JsonQueryGenerator::GenerateJson()
     else if (qt == QueryType::USER_DATA)
     {
         resultJson["method"] = "account.status";
-        resultJson["params"]["apiKey"] = apiKey;
         resultJson["params"]["timestamp"] = std::any_cast<std::time_t>(arguements[0]);
+        resultJson["params"]["apiKey"] = apiKey;
+        
 
         const auto paramsString = GetParamsStringToSign(resultJson["params"]);
         resultJson["params"]["signature"] = CalculateSignature(paramsString);
-
     }
     else if (qt == QueryType::SERVER_TIME)
     {
         resultJson["method"] = "time";
     }
+    else if (qt == QueryType::BUY)
+    {
+        resultJson["method"] = "order.place";
+        resultJson["params"]["apiKey"] = apiKey;
+        resultJson["params"]["symbol"] = std::any_cast<std::string>(arguements[0]);
+        resultJson["params"]["side"] = "BUY";
+        resultJson["params"]["type"] = "LIMIT";
+        resultJson["params"]["timeInForce"] = "GTC";
+        resultJson["params"]["price"] = std::any_cast<double>(arguements[1]);
+        resultJson["params"]["quantity"] = std::any_cast<double>(arguements[2]);
+        resultJson["params"]["timestamp"] = std::any_cast<time_t>(arguements[3]);
+    }
+}
 
     // implement other methods later
-}
+
 
 std::string JsonQueryGenerator::CalculateSignature(const std::string& paramsString) const
 {
@@ -49,25 +62,31 @@ std::string JsonQueryGenerator::CalculateSignature(const std::string& paramsStri
 
 std::string JsonQueryGenerator::GetParamsStringToSign(const nlohmann::json& paramsSection)
 {
+    std::map<std::string, std::string> resMap;
+    for (const auto& [param, value] : paramsSection.items())
+    {
+        std::string valueStr;
+        if (value.is_string())
+            value.get_to(valueStr);
+        else if (value.is_number_integer()) 
+        {
+            long long tempNumber;
+            value.get_to(tempNumber);
+            valueStr = std::to_string(tempNumber);
+        }
+        else if (value.is_number_float())
+        {
+            double tempNumber;
+            value.get_to(tempNumber);
+            valueStr = std::to_string(tempNumber);
+        } 
+        resMap[param] = valueStr;
+    }
     std::string paramsString {};
-        for (const auto& [param, value] : paramsSection.items())
+        for (const auto& [param, value] : resMap)
         {   
-            std::string valueStr;
-            if (value.is_string())
-                value.get_to(valueStr);
-            else if (value.is_number_integer()) 
-            {
-                long long tempNumber;
-                value.get_to(tempNumber);
-                valueStr = std::to_string(tempNumber);
-            }
-            else if (value.is_number_float())
-            {
-                double tempNumber;
-                value.get_to(tempNumber);
-                valueStr = std::to_string(tempNumber);
-            } 
-            paramsString += param + "=" + valueStr + "&";
+            
+            paramsString += param + "=" + value + "&";
         }
         paramsString.pop_back(); // erase trailing '&'
         return paramsString;
