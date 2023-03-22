6c1bf413dc64132582dd946f65081f7fb6e50b06
Timur Akhmadaliev <timur600@gmail.com>
2023-03-22 17:43:20 +0400

    Add fintech-test.cpp

---
+++ src/fintech-test.cpp
@@ -0,0 +1,44 @@
+#include "CommonTypes.h"
+#include "Trader.h"
+
+#include <argumentum/argparse.h>
+
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <ixwebsocket/IXUserAgent.h>
+#include <nlohmann/json.hpp>
+
+#include <iostream>
+#include <thread>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    using namespace BuySellRepeat_NS;
+    using namespace argumentum;
+
+    std::string apiKey;
+    std::string secretKey;
+    TradingParameters tradingParameters;
+
+    auto parser = argument_parser{};
+    parser.config().program(argv[0]);
+    auto programParams = parser.params();
+    parser.config().program(argv[0]).description("Buy, sell, repeat");
+    programParams.add_parameter(apiKey, "--api_key").nargs(1).help("Your API key");
+    programParams.add_parameter(secretKey, "--secret_key").nargs(1).help("Your secret key");
+    auto pOptions = std::make_shared<TradingParameters>();
+    programParams.add_parameters(pOptions);
+    if (!parser.parse_args(argc, argv, 1))
+        throw std::logic_error("Incorrect arguements set");
+    
+    std::cout << tradingParameters.myCurrency;
+    std::cout << tradingParameters.tradingCurrency;
+
+    WebIO wio("wss://testnet.binance.vision/ws-api/v3", apiKey, secretKey);
+    Display disp(std::cout);
+    Trader tr(*pOptions, wio, disp);
+    tr.StartTrading();
+
+    return 0;
+}
